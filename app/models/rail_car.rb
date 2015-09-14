8f451969b65411e7e4b4640e6898eafbdf5da8b9
Carmelo <lattuca@yahoo.com>
2015-09-13 20:57:47 -0700

    fixed the easy requirements

--- app/models/rail_car.rb
+++ app/models/rail_car.rb
@@ -1,4 +1,8 @@
 class RailCar < ActiveRecord::Base
   belongs_to :vendor
   belongs_to :purchaseorder
-end
+
+  validates :railcar_nbr, presence: true
+  validates_numericality_of :bol_nbr, :greater_than_or_equal_to => 10000000, :less_than_or_equal_to => 10000000000, :message =>  "needs to be between 8-10 digits"
+
+ end
--- app/models/trailer.rb
+++ app/models/trailer.rb
@@ -1,3 +1,11 @@
 class Trailer < ActiveRecord::Base
   belongs_to :purchaseorder
+
+  validates :trailer_nbr, presence: true, uniqueness: true
+  validates :driver_name, presence: true
+  validates :bol_nbr, presence: true
+  validates_numericality_of :bol_nbr, :greater_than_or_equal_to => 10000000, :less_than_or_equal_to => 10000000000, :message =>  "needs to be between 8-10 digits"
+  validates :weight_lbs, presence: true
+
+
 end
