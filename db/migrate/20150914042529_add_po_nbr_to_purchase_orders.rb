93ca396bd51e0947ec92382712fccad9ea561710
Carmelo <lattuca@yahoo.com>
2015-09-18 21:11:28 -0700

    purchase order problem with heroku db migrate

--- db/migrate/20150914042529_add_po_nbr_to_purchase_orders.rb
+++ db/migrate/20150914042529_add_po_nbr_to_purchase_orders.rb
@@ -1,4 +1,4 @@
-class AddPoNbrToPurchaseOrder < ActiveRecord::Migration
+class AddPoNbrToPurchaseOrders < ActiveRecord::Migration
   def change
     add_column :purchase_orders, :po_nbr, :integer
   end
